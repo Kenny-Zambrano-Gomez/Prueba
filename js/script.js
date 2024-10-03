2da1276cca3d13eb376a5c35d6f6c78dbab903fa
Kenny-Zambrano-Gomez <175513242+Kenny-Zambrano-Gomez@users.noreply.github.com>
2024-10-02 21:26:56 -0500

    Primer Commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const mobileMenu = document.getElementById('mobile-menu');
+const navList = document.querySelector('.nav-list');
+
+
+mobileMenu.addEventListener('click', () => {
+    navList.classList.toggle('active');
+});
+
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggle = document.getElementById('theme-toggle');
+    const currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme === 'dark') {
+        document.body.classList.add('dark-mode');
+        document.querySelector('header').classList.add('dark-mode');
+        themeToggle.innerHTML = '<i class="fas fa-sun"></i>';
+    }
+
+    themeToggle.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        const header = document.querySelector('header');
+        header.classList.toggle('dark-mode');
+
+        if (document.body.classList.contains('dark-mode')) {
+            themeToggle.innerHTML = '<i class="fas fa-sun"></i>';
+            localStorage.setItem('theme', 'dark');
+        } else {
+            themeToggle.innerHTML = '<i class="fas fa-moon"></i>';
+            localStorage.setItem('theme', 'light');
+        }
+    });
+});
+
